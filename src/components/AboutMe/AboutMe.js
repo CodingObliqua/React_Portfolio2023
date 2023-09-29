1937edca4c6fe888a23b1b1187ad843854cba0ac
codingobliqua <andres.asencios10@gmail.com>
2023-09-29 03:10:41 -0700

    created aboutme.js and header.js

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,17 @@
+// src/components/AboutMe/AboutMe.js
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <section>
+            <h2> About Me</h2>
+            <img src="/assets/images/pictureofme.jpeg" alt="photo of me" />
+            <p>
+                wabble de dabble de 
+                
+            </p>
+        </section>
+    );
+};
+
+export default AboutMe;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+// src/components/Header/Header.js
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            {/* Display Name*/}
+            <h1>Andres Asencios</h1>
+
+            {/*Navigation links will go here */}
+        </header>
+    );
+};
+
+export default Header;
