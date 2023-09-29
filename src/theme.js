cd90ee14093235be477550605793b13f6fc87240
codingobliqua <andres.asencios10@gmail.com>
2023-09-29 13:06:41 -0700

    set up theme and index.js

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+// theme.js 
+
+const theme = {
+    colors: {
+        primary: '#00ffa3db', // Primary color here
+        secondary: '#a980d1b3', // Secondary color here
+    },
+    fonts: {
+        body: 'Arial, sans-serif', // Font family here
+        heading: 'Helvetica, sans-serif', // font family for heading
+    },
+
+    // more theme properties go here
+
+
+};
+
+export default theme;
