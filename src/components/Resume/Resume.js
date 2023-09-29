f7856a1b25653ec7ca0c08f8d0adc68ac1e03630
codingobliqua <andres.asencios10@gmail.com>
2023-09-29 03:07:51 -0700

    Created resume.js boilerplate

---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,17 @@
+// src/components/Resume/Resume.js
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <section>
+            <h2>Resume</h2>
+            <a href="src\assets\RESUME_cv (2).pdf" download>
+                Download Resume
+            </a>
+            <ul>
+                <li>Proficiency 1</li>
+                <li>Proficiency 2</li>
+            </ul>
+        </section>
+    );
+};
