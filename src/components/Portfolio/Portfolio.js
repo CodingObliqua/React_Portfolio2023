04c3c94dee0825156ecb8724e3b3baffc8505be9
codingobliqua <andres.asencios10@gmail.com>
2023-10-18 04:08:34 -0700

    added photos

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -29,6 +29,7 @@ const SectionTitle = styled.h2`
   margin-bottom: 20px;
 `;
 
+
 const Portfolio = () => {
     const projects = [
         {
@@ -36,42 +37,42 @@ const Portfolio = () => {
             description: "This is my 2023 portfolio made using HTML and CSS!",
             deployedLink: "https://github.com/CodingObliqua/Portfolio-2023",
             githubLink: "https://codingobliqua.github.io/Portfolio-2023/",
-            imageSrc: "",
+            imageSrc: require("../../assets/html_port.JPG"),
         },
         {
             title: "JavaScript HTML CSS Project (Server-side API)",
             description: "I worked in a group to build a Interactive Front-End project/server-side APIs! ",
             deployedLink: "https://codingobliqua.github.io/BerkeleyRacingClub/",
             githubLink: "https://github.com/CodingObliqua/BerkeleyRacingClub",
-            imageSrc: "",
+            imageSrc: require("../../assets/berkeley_racing.JPG"),
         },
         {
             title: "Project 2",
             description: "Created a project to utilized CRUD routes and Login Authorization ",
             deployedLink: "https://horse-creator-app-9839a5a7a948.herokuapp.com/",
             githubLink: "https://github.com/CodingObliqua/Horse_creator",
-            imageSrc: "",
+            imageSrc: require("../../assets/horse_creat.JPG"),
         },
         {
             title: "React Portfolio",
             description: "Developed a portfolio utilizing React ",
             deployedLink: "https://codingobliqua.github.io/React_Portfolio2023/",
             githubLink: "https://github.com/CodingObliqua/React_Portfolio2023",
-            imageSrc: "",
+            imageSrc: require("../../assets/react_port.JPG"),
         },
         {
             title: "Timed Quiz (HTML,CSS,JS)",
             description: "Developed an Art gallery app using MERN stack ",
             deployedLink: "https://codingobliqua.github.io/fuzzy-winner/",
             githubLink: "https://github.com/CodingObliqua/fuzzy-winner",
-            imageSrc: "",
+            imageSrc: require("../../assets/js_quiz.JPG"),
         },
         {
             title: "Project 3",
             description: "Developed an Art gallery app using MERN stack ",
             deployedLink: "https://codingobliqua.github.io/BerkeleyRacingClub/",
             githubLink: "https://github.com/CodingObliqua/BerkeleyRacingClub",
-            imageSrc: "",
+            imageSrc: require("../../assets/fish.jpg"),
         },
 
 
@@ -91,6 +92,7 @@ const Portfolio = () => {
                 description={project.description}
                 deployedLink={project.deployedLink}
                 githubLink={project.githubLink}
+                imageSrc={project.imageSrc}
                 />
             ))}
         </div>
